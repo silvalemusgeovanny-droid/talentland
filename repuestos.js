8da418e6659098444d20172c431cc86923add8dd
silvalemusgeovanny-droid <silvalemusgeovanny@gmail.com>
2026-05-25 13:50:47 -0600

    Update repuestos.js

--- repuestos.js
+++ repuestos.js
@@ -157,7 +157,10 @@ function renderParts() {
           <td>${part.supplier}</td>
           <td>${formatCurrency(part.price)}</td>
           <td>${part.stock}</td>
-          <td>   <button class="edit-button" type="button" data-id="${part.id}">Editar</button>   <button class="delete-button" type="button" data-id="${part.id}">Eliminar</button> </td>
+          <td>   <button class="edit-button" type="button" data-id="${part.id}">Editar</button>   <td>
+  <button class="edit-button" type="button" data-id="${part.id}">Editar</button>
+  <button class="delete-button" type="button" data-id="${part.id}">Eliminar</button>
+</td>
         </tr>
       `,
     )
